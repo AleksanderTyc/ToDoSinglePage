ec92474fda9eb8d24961efe2157d7ff471c14006
Aleks Tyc <aleks.tyc@gmail.com>
2025-11-21 11:30:57 +0100

    Code and comments tidy up

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ let myTasks;
 let eventMode = -1;
 
 function getData() {
-    // myTasks = JSON.parse(localStorage.getItem("myTasks")) || [{ title: "ab", details: "cd", due: "2025-11-19T13:33", status: "Complete" }];
     myTasks = JSON.parse(localStorage.getItem("myTasks")) || [];
 }
 
@@ -18,49 +17,9 @@ function populateTasks() {
             ns: 'Not started', ip: 'In progress', cp: 'Complete', cn: 'Cancelled'
         };
         ['ns', 'ip', 'cp', 'cn'].forEach(taskStatus => {
-            console.log(`*** DIAG *** Setting status opt-${taskStatus}-${index}`, document.getElementById(`opt-${taskStatus}-${index}`) );
             document.getElementById(`opt-${taskStatus}-${index}`).selected = (task.status === optionsMap[taskStatus]);
         });
     });
-    /*
-    const tasksHTML = `
-    <div class="entry-head" id="entry-head-${index}">
-        <table>
-            <tr>
-                <td id="td-title-${index}>${task.title}</td>
-                    <td id="td-due-${index}">${task.due}</td>
-                    <td id="td-status-${index}">${task.status}</td>
-            </tr>
-        </table>
-    </div>
-    <div class="entry-form" style="display: none;" id="entry-form-${index}">
-        <p>
-            <label>Title</label>
-            <input type="text" id="txt-title-${index}" value="${task.title}" placeholder="Enter Title">
-        </p>
-        <p>
-            <label>Details</label>
-            <textarea id="txt-dtls-${index}" rows="4" columns="15" placeholder="Enter Details">${task.details}</textarea>
-        </p>
-        <p>
-            <label>Due Date</label>
-            <input type="datetime-local" id="dt-due-${index}" value="${task.due}">
-        </p>
-        <p>
-            <label>Status</label>
-            <select id="sel-status-${index}">
-                    <option id="opt-ns-${index}" ${task.status === 'Not started' ? 'selected' : ''}>Not started</option>
-                    <option id="opt-ip-${index}" ${task.status === 'In progress' ? 'selected' : ''}>In progress</option>
-                    <option id="opt-cp-${index}" ${task.status === 'Complete' ? 'selected' : ''}>Complete</option>
-                    <option id="opt-cn-${index}" ${task.status === 'Cancelled' ? 'selected' : ''}>Cancelled</option>
-            </select>
-        </p>
-        <p>
-            <button class="btn-dsm" id="btn-dsm-${index}">Dismiss</button>
-            <button class="btn-save" id="btn-save-${index}">Save changes</button>
-        </p>
-    </div>`;
-*/
 }
 
 function setInitialEventListeners() {
@@ -90,7 +49,6 @@ function executeOnLoad() {
 
 function onClickOnHead(headIndex) {
     if (eventMode === -1) {
-        console.log(`*** DIAG *** Called at line ${headIndex}`);
         const currHead = document.getElementById(`entry-head-${headIndex}`);
         currHead.style.display = 'none';
         const currForm = document.getElementById(`entry-form-${headIndex}`);
@@ -108,13 +66,8 @@ function onOKCancelClick(headIndex, buttonOK) {
             due: document.getElementById(`dt-due-${headIndex}`).value,
             status: document.getElementById(`sel-status-${headIndex}`).value
         };
-        console.log(`*** INFO *** Save changes clicked on ${headIndex}, saving data`);
-        console.log(`*** INFO *** Save changes data object`, currData);
-        console.log(`*** INFO *** Save changes END`);
         myTasks[headIndex] = currData;
         localStorage.setItem("myTasks", JSON.stringify(myTasks));
-    } else {
-        console.log(`*** INFO *** Dismiss changes clicked on ${headIndex}, closing the form`);
     }
     const currHead = document.getElementById(`entry-head-${headIndex}`);
     currHead.style.display = 'block';
@@ -126,11 +79,8 @@ function onOKCancelClick(headIndex, buttonOK) {
 function onNewClick() {
     if (eventMode === -1) {
         newTaskIndex = myTasks.length;
-        console.log(`*** INFO *** + clicked, adding New Task index ${newTaskIndex}`);
         myTasks.push({ title: "", details: "", due: "", status: "Not started" });
-        // const tasksContainer = document.getElementById('contr-tasks');
-        // tasksContainer.innerHTML += renderSingleTask(newTaskIndex);
-        document.getElementById('contr-tasks').appendChild( renderSingleTask(newTaskIndex) );
+        document.getElementById('contr-tasks').appendChild(renderSingleTask(newTaskIndex));
         populateTasks();
         setEventListenersOnIndex(newTaskIndex);
         // format new Date() into 2025-11-19T13:33
